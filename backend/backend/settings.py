23c0ccb6dee8098b1b7f649ef40925bfb53e373e
neolestrix <johnlester.kortmann@epneo.com>
2025-02-21 23:01:57 +0100

    added models and ran migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job_portal',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class JobApplication(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applicant_name = models.CharField(max_length=255)
+    applicant_email = models.EmailField()
+    resume = models.FileField(upload_to='resumes/')
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.applicant_name} applied for {self.job.title}"
